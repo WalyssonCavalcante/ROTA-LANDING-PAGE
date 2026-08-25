3ae119afa4deaa1c5421a01c9e7f2f17f168f924
WalyssonCavalcante <wallysson.cavalcante@gmail.com>
2026-08-25 09:26:11 -0300

    Fix: about-page

--- src/app/pages/home/components/services/services.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-services',
-  styleUrl: './services.scss',
-  templateUrl: './services.html',
-})
-export class Services {}
